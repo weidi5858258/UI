fdcd6f3eb4813dfaf3cd8cf50615ceb482abe6d4
weidi5858258 <weidi5858258@sina.com>
2016-06-26 01:40:07 +0800

    使用自己的颜色 命名风格

---
+++ app/src/main/java/com/weidi/ui/customview/WScrollView.java
@@ -0,0 +1,25 @@
+package com.weidi.ui.customview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+/**
+ * Created by root on 16-6-25.
+ */
+
+public class WScrollView extends ScrollView {
+
+    public WScrollView(Context context) {
+        super(context);
+    }
+
+    public WScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
---
+++ app/src/main/java/com/weidi/ui/customview/WTextView.java
@@ -0,0 +1,26 @@
+package com.weidi.ui.customview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+/**
+ * Created by root on 16-6-25.
+ */
+
+public class WTextView extends TextView {
+
+    public WTextView(Context context) {
+        super(context);
+    }
+
+    public WTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
